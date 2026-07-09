dd297e731b74c3a80deb448d10d7f70120a16ad2
shibaken <katsufumi.shibata@dbca.wa.gov.au>
2026-07-09 10:18:17 +0800

    Add get_privacy_policy_url template tag to url_helpers

--- wildlifelicensing/apps/main/templatetags/url_helpers.py
+++ wildlifelicensing/apps/main/templatetags/url_helpers.py
@@ -1,6 +1,12 @@
+from django.conf import settings
 from django.template.defaulttags import register
 
 
 @register.filter
 def get_url_filename(url):
     return url[url.rfind('/') + 1:]
+
+
+@register.simple_tag
+def get_privacy_policy_url():
+    return settings.PRIVACY_POLICY_URL
